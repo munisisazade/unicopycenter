cedce084fda586ebb97e8aec784c5d4e7f85790c
Munis Isazade <munisisazade@gmail.com>
2017-02-02 21:44:12 +0400

    fix model

--- center/models.py
+++ center/models.py
@@ -16,6 +16,7 @@ class Orders(models.Model):
     number = models.CharField(max_length=255,verbose_name="Nomre")
     text = models.TextField(verbose_name="Əlavə məlumat")
     types = models.IntegerField(choices=tipi,default=1)
+    date = models.DateTimeField(null=True,blank=True,editable=True, auto_now_add=True)
 
     class Meta:
         ordering = ('-id',)
