6480f567856bd79e47fe7babaefe826a243dc188
Munis Isazade <munisisazade@gmail.com>
2017-02-04 03:16:37 +0400

    add

--- center/views.py
+++ center/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class IndexRequestView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexRequestView, self).get_context_data(**kwargs)
+        context['html_lang'] = self.request.get_full_path()[1:3]
+        return context
