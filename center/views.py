c1153a82f6ec0eac10aaa9ebf5142c0293d30919
Munis Isazade <munisisazade@gmail.com>
2017-02-09 17:07:18 +0400

    Update unicopycenter

--- center/views.py
+++ center/views.py
@@ -9,9 +9,14 @@ class IndexRequestView(TemplateView,FormView):
 
 
     def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST,request.FILES)
+        if form.is_valid():
+            pass
         pass
+
 
     def get_context_data(self, **kwargs):
         context = super(IndexRequestView, self).get_context_data(**kwargs)
         context['html_lang'] = self.request.get_full_path()[1:3]
         return context
+
