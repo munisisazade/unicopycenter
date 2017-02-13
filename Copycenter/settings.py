bfe3f9aee2e175d46fbeda4bfd248079e4b88e48
Munis Isazade <munisisazade@gmail.com>
2017-02-13 23:42:43 +0400

    fix template errors

--- Copycenter/settings.py
+++ Copycenter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$9ewus3gbfad7$k@5q1kpz=^xmn&6db2=tf1+_&9%%9wha@f7m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
